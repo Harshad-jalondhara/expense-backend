6c3ef61954c369252cec3da39bcccf339be75a9a
Harshad <jalondharaharshad007@gmail.com>
2026-02-22 15:47:00 +0530

    fix-5

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const ConnectDB = async() => {
     try {
-        await mongoose.connect(`${process.env.MONGODB_URL}/expense`);
+        await mongoose.connect(process.env.MONGODB_URL);
         console.log("MongoDB Connect");
     } catch (error) {
         console.log("MongoDB Conneting Error : ", error);
